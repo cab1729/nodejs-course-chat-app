ef064bf979b49f11b92a70090e974702cffb3987
Jose Menes <jmenes1729@gmail.com>
2018-03-12 18:02:17 -0400

    Added new user greeting and alert for connected users

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
@@ -23,6 +34,12 @@ io.on('connection', (socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
   socket.on('disconnect', () => {
     console.log('user disconnected from server');

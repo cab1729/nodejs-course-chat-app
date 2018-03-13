c461ed5878e88e62e08a68547179f807786e168c
Jose Menes <jmenes1729@gmail.com>
2018-03-13 19:08:02 -0400

    Added form for sending messages and list of incoming

--- server/server.js
+++ server/server.js
@@ -19,10 +19,11 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (newMessage) => {
+  socket.on('createMessage', (newMessage, callback) => {
     console.log('createMessage', newMessage);
 
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    callback();
 
     // socket.broadcast.emit('newMessage', {
     //   from: newMessage.from,
